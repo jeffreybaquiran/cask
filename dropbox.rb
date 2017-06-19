864c717877764743873aff2b68066ce4b4f79f65
jeffreybaquiran <mail@jeffreybaquiran.com>
2017-06-19 16:43:24 +0800

    commit

--- dropbox.rb
+++ dropbox.rb
@@ -1,6 +1,6 @@
 cask 'dropbox' do
   version '28.4.14'
-  url 'https://www.dropbox.com/downloading?build=28.4.14&plat=mac&type=full'
+  url 'https://www.dropbox.com/download?plat=mac&full=1'
   dmg_file 'Dropbox 28.4.14.dmg'
   name 'Dropbox'
   app 'Dropbox.app'
