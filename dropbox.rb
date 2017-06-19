4e3772bb968e87e6ae926c49ff325f726c948583
jeffreybaquiran <mail@jeffreybaquiran.com>
2017-06-19 13:19:10 +0800

    Create dropbox.rb

---
+++ dropbox.rb
@@ -0,0 +1,8 @@
+cask 'dropbox' do
+  version :'28.4.14'
+  
+  url 'https://www.dropbox.com/downloading?build=#{version}&plat=mac&type=full'
+  name 'Dropbox'
+  filename 'Dropbox 28.4.14.dmg'
+ 
+  app 'Dropbox.app'
