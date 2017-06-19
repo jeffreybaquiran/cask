172cf16035d49c5a8554b75768bd761bb61ffad1
jeffreybaquiran <mail@jeffreybaquiran.com>
2017-06-19 13:28:53 +0800

    Delete dropbox.rb

--- dropbox.rb
+++
@@ -1,8 +0,0 @@
-cask 'dropbox' do
-  version '28.4.14'
-  
-  url 'https://www.dropbox.com/downloading?build=#{version}&plat=mac&type=full'
-  name 'Dropbox'
-  filename 'Dropbox #{version}.dmg'
- 
-  app 'Dropbox.app'
