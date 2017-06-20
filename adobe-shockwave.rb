290b1960c5455033e2d5c8b4ad178f22a6aac554
jeffreybaquiran <mail@jeffreybaquiran.com>
2017-06-20 10:12:43 +0800

    commit

---
+++ adobe-shockwave.rb
@@ -0,0 +1,7 @@
+cask 'adobe-shockwave' do
+  version :12.2.5.195
+
+  dmg_file 'Shockwave_Installer_Full_12.2.5.195.dmg'
+  name 'Adobe Shockwave'
+  googledriveid '0B_TzDxaGDaKreHpZdk9YQjRfY0E'
+  pkg 'Shockwave_Installer_Full.pkg'
